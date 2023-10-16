64a797a1af2d82d51153de660dc4d9c526f1c192
GabeYanes1217 <gabriel.yanes1217@outlook.com>
2023-10-15 21:45:16 -0300

    mais operadores

---
+++ Alura.JS/template_strings.js
@@ -0,0 +1,9 @@
+const sobrenome = "Yanes";
+const nome = "Gabriel "
+const idade = 2023 - 2002
+const municipio = "Baixada Santista"
+// const introducao = "Meu nome é " + nome + sobrenome + " moro na " + municipio + " e tenho " + idade + " anos";
+
+const introducao = `Meu nome é ${nome}${sobrenome}, tenho ${idade} anos e nasci na ${municipio}`;
+
+console.log(introducao)
---
+++ Alura.JS/ternario.js
@@ -0,0 +1,13 @@
+const idadeMinina = 18;
+let idadeCliente = 17;
+
+// if(idadeCliente >= idadeMinina){
+//     console.log('você já é de maior, portanto já pode beber, parabéns!!')
+// }else{
+//     console.log('tu és de menor, larga essa cerveja e vai estudar')
+// }
+
+
+            //condição                    //true     //false
+console.log(idadeCliente >= idadeMinina ? "cerveja" : "suco")
+
